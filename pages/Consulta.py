f6f9e90e593dcbea8723c1f35de24d5cbd548aa7
Marco Antonio Bentancor <marco.bentancor@gmail.com>
2024-07-18 15:30:34 -0300

    Add files via upload

--- pages/Consulta.py
+++ pages/Consulta.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-dados = pd.read_csv('projeto_pratico/clientes.csv')
+dados = pd.read_csv('clientes.csv')
 
 st.title('Clientes Cadastrados')
 st.dataframe(dados)
